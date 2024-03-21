0f00766ede342467ebc4e6b9c7f27c9f0d5e683b
Abhinav Dixit <abhi@Abhinavs-MacBook-Air.local>
2024-03-21 15:58:57 -0500

    added project files

---
+++ src/main/java/org/improving/workshop/project/CustomerAttendingShows.java
@@ -0,0 +1,180 @@
+package org.improving.workshop.project;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.*;
+import org.improving.workshop.Streams;
+import org.improving.workshop.samples.PurchaseEventTicket;
+import org.msse.demo.mockdata.customer.address.Address;
+import org.msse.demo.mockdata.customer.profile.Customer;
+import org.msse.demo.mockdata.music.event.Event;
+import org.msse.demo.mockdata.music.ticket.Ticket;
+import org.msse.demo.mockdata.music.venue.Venue;
+import org.springframework.kafka.support.serializer.JsonSerde;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.apache.kafka.streams.state.Stores.persistentKeyValueStore;
+import static org.improving.workshop.Streams.*;
+import static org.improving.workshop.Streams.SERDE_TICKET_JSON;
+
+@Slf4j
+public class CustomerAttendingShows {
+    public static final JsonSerde<VenueAddress> SERDE_VENUE_ADDRESS_JSON =
+            new JsonSerde<>(CustomerAttendingShows.VenueAddress.class);
+
+    public static final JsonSerde<ArrayList<Customer>> SERDE_FINAL_CUSTOMER_DETAILS_JSON =
+            new JsonSerde<>(ArrayList.class);
+
+    public static final String OUTPUT_TOPIC = "customer-attending-shows-output";
+
+
+    public static void main(String[] args) {
+        final StreamsBuilder builder = new StreamsBuilder();
+
+        // configure the processing topology
+        configureTopology(builder);
+
+        // fire up the engines
+        startStreams(builder);
+    }
+
+    static void configureTopology(final StreamsBuilder builder) {
+
+
+
+        KTable<String, Customer> customerTable = builder
+                .table(
+                        TOPIC_DATA_DEMO_CUSTOMERS,
+                        Materialized
+                                .<String, Customer>as(persistentKeyValueStore("customers"))
+                                .withKeySerde(Serdes.String())
+                                .withValueSerde(Streams.SERDE_CUSTOMER_JSON)
+                );
+
+        KTable<String, Event> eventsTable = builder
+                .table(
+                        TOPIC_DATA_DEMO_EVENTS,
+                        Materialized
+                                .<String, Event>as(persistentKeyValueStore("events"))
+                                .withKeySerde(Serdes.String())
+                                .withValueSerde(SERDE_EVENT_JSON)
+                );
+
+
+        var addressOfCustomerTable =  builder
+                .stream(TOPIC_DATA_DEMO_ADDRESSES, Consumed.with(Serdes.String(), SERDE_ADDRESS_JSON))
+                .filter((k,v) -> v.customerid() != null)
+                .selectKey((key, value) -> value.customerid())
+                .toTable(Named.as("customer-only-address"),
+                        Materialized
+                                .<String, Address>as(persistentKeyValueStore("customer-only-address"))
+                                .withKeySerde(Serdes.String())
+                                .withValueSerde(SERDE_ADDRESS_JSON));
+
+        var addressOfVenueTable =  builder
+                .stream(TOPIC_DATA_DEMO_ADDRESSES, Consumed.with(Serdes.String(), SERDE_ADDRESS_JSON))
+                .peek((k,v) -> log.info("Value of address of venue before filter data k - {} and v - {}", k, v))
+                .filter((k,v) -> v.customerid() == null)
+                .peek((k,v) -> log.info("Value of address of venue after filter data k - {} and v - {}", k, v))
+                .selectKey((key, value) -> value.customerid())
+                .toTable(Named.as("venue-only-address"),
+                        Materialized
+                                .<String, Address>as(persistentKeyValueStore("venue-only-address"))
+                                .withKeySerde(Serdes.String())
+                                .withValueSerde(SERDE_ADDRESS_JSON));
+
+        var venueAndAddressTable = builder
+                .stream(TOPIC_DATA_DEMO_VENUES, Consumed.with(Serdes.String(), SERDE_VENUE_JSON))
+                .peek((k,v) -> log.info("Value of venue table data k - {} and v - {}", k, v))
+                .selectKey((k,v) -> v.addressid())
+                .peek((k,v) -> log.info("Value of venue table data k - {} and v - {}", k, v))
+                .join(addressOfVenueTable, (addressId, venue, address) -> new VenueAddress(venue, address))
+                .peek((k,v) -> log.info("Value of venue and address table after join data k - {} and v - {}", k, v))
+                .selectKey((k,v) -> v.venue.id())
+                .toTable(Named.as("venue-and-address"),
+                        Materialized
+                                .<String, VenueAddress>as(persistentKeyValueStore("venue-and-address"))
+                                .withKeySerde(Serdes.String())
+                                .withValueSerde(SERDE_VENUE_ADDRESS_JSON));
+
+//        builder.stream(TOPIC_DATA_DEMO_TICKETS, Consumed.with(Serdes.String(), SERDE_TICKET_JSON))
+//                .peek((k,v)-> log.info("Entering Stream with key - {} and value - {}", k, v))
+//                .selectKey((k,v) -> v.customerid())
+//                .join(customerTable, (customerId, ticket, customer) -> new TicketCustomer(ticket, customer))
+//                .peek((k,v)-> log.info("Stream after joining customer table - {} and value - {}", k, v))
+//                .join(addressOfCustomerTable, (customerId, ticketCustomer, addressOfCustomer) -> new TicketCustomerAddress(ticketCustomer, addressOfCustomer))
+//                .peek((k,v)-> log.info("Stream after joining address of customer table - {} and value - {}", k, v))
+//                .selectKey((k, v) -> v.ticketCustomer.ticket.eventid())
+//                .join(eventsTable, (eventId, ticketCustomerAddress, event) -> new TicketCustomerAddressEvent(ticketCustomerAddress, event))
+//                .peek((k,v)-> log.info("Stream after joining events table - {} and value - {}", k, v))
+//                .selectKey((k, v) -> v.event.venueid())
+//                .join(venueAndAddressTable, (venueId, ticketCustomerAddressEvent, venueAndAddress) -> new TicketCustomerAddressEventVenueAndAddress(ticketCustomerAddressEvent, venueAndAddress))
+//                .peek((k,v)-> log.info("Stream after joining venue and Address table - {} and value - {}", k, v))
+//                .selectKey((k, v) -> v.ticketCustomerAddressEvent.ticketCustomerAddress.addressOfCustomer.state()+v.ticketCustomerAddressEvent.event.id())
+//                .filter((k,v) -> !v.ticketCustomerAddressEvent.ticketCustomerAddress.addressOfCustomer.state().equals(v.venueAddress.address.state()))
+//                .peek((k,v)-> log.info("Stream after filtering out different state customers - {} and value - {}", k, v))
+//                .groupByKey()
+//                .aggregate(ArrayList<Customer>::new,
+//                        (key, oldValue, list)->{
+//                                    list.add(oldValue.ticketCustomerAddressEvent.ticketCustomerAddress.ticketCustomer.customer);
+//                    return list;
+//                        })
+//                .toStream()
+//                        .to(OUTPUT_TOPIC, Produced.with(Serdes.String(), SERDE_FINAL_CUSTOMER_DETAILS_JSON));
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class VenueAddress {
+        private Venue venue;
+        private Address address;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class TicketCustomer {
+        private Ticket ticket;
+        private Customer customer;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class TicketCustomerAddress {
+        private TicketCustomer ticketCustomer;
+        private Address addressOfCustomer;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class TicketCustomerAddressEvent {
+        private TicketCustomerAddress ticketCustomerAddress;
+        private Event event;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class TicketCustomerAddressEventVenueAndAddress {
+        private TicketCustomerAddressEvent ticketCustomerAddressEvent;
+        private VenueAddress venueAddress;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class FinalCustomerDetails {
+        private Customer customer;
+    }
+}
---
+++ src/test/groovy/org/improving/workshop/project/CustomerAttendingShowsSpec.groovy
@@ -0,0 +1,139 @@
+package org.improving.workshop.project
+
+import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.streams.StreamsBuilder
+import org.apache.kafka.streams.TestInputTopic
+import org.apache.kafka.streams.TestOutputTopic
+import org.apache.kafka.streams.TopologyTestDriver
+import org.improving.workshop.Streams
+import org.improving.workshop.exercises.stateless.AddressSortAndStringify
+import org.msse.demo.mockdata.customer.address.Address
+import org.msse.demo.mockdata.customer.profile.Customer
+import org.msse.demo.mockdata.music.event.Event
+import org.msse.demo.mockdata.music.ticket.Ticket
+import org.msse.demo.mockdata.music.venue.Venue
+import spock.lang.Specification
+
+class CustomerAttendingShowsSpec extends Specification {
+
+    TopologyTestDriver driver
+
+    // inputs
+    TestInputTopic<String, Address> addressInputTopic
+    TestInputTopic<String, Event> eventInputTopic
+    TestInputTopic<String, Ticket> ticketInputTopic
+    TestInputTopic<String, Customer> customerInputTopic
+    TestInputTopic<String, Venue> venueInputTopic
+
+    // outputs - addressid, address (string)
+    TestOutputTopic<String, String> outputTopic
+
+
+    def 'setup'() {
+        // instantiate new builder
+        StreamsBuilder streamsBuilder = new StreamsBuilder()
+
+        // build the AddressSortAndStringify topology (by reference)
+        CustomerAttendingShows.configureTopology(streamsBuilder)
+
+        // build the TopologyTestDriver
+        driver = new TopologyTestDriver(streamsBuilder.build(), Streams.buildProperties())
+
+        venueInputTopic = driver.createInputTopic(
+                Streams.TOPIC_DATA_DEMO_VENUES,
+                Serdes.String().serializer(),
+                Streams.SERDE_VENUE_JSON.serializer()
+        )
+
+        customerInputTopic = driver.createInputTopic(
+                Streams.TOPIC_DATA_DEMO_CUSTOMERS,
+                Serdes.String().serializer(),
+                Streams.SERDE_CUSTOMER_JSON.serializer()
+        )
+
+        ticketInputTopic = driver.createInputTopic(
+                Streams.TOPIC_DATA_DEMO_TICKETS,
+                Serdes.String().serializer(),
+                Streams.SERDE_TICKET_JSON.serializer()
+        )
+
+        eventInputTopic = driver.createInputTopic(
+                Streams.TOPIC_DATA_DEMO_EVENTS,
+                Serdes.String().serializer(),
+                Streams.SERDE_EVENT_JSON.serializer()
+        )
+
+        addressInputTopic = driver.createInputTopic(
+                Streams.TOPIC_DATA_DEMO_ADDRESSES,
+                Serdes.String().serializer(),
+                Streams.SERDE_ADDRESS_JSON.serializer()
+        )
+
+        outputTopic = driver.createOutputTopic(
+                CustomerAttendingShows.OUTPUT_TOPIC,
+                Serdes.String().deserializer(),
+                Serdes.String().deserializer()
+        )
+    }
+
+    def 'cleanup'() {
+        // if this close doesn't run (test fails halfway through), subsequent tests may start on old state
+        // run the test and let it cleanup, then run the test again.
+        driver.close()
+    }
+
+    def "customers from same state attending event in a different state"() {
+        given: 'a set of customers'
+        def cust1 = new Customer("cust-123", "PREMIUM", "M", "John", "Steven", "James", "JSJ", "", "", "1989-01-20", "2022-01-02")
+        def cust2 = new Customer("cust-456", "PREMIUM", "M", "Jane", "Jo", "James", "JJJ", "", "", "1990-01-20", "2022-01-02")
+        def cust3 = new Customer("cust-567", "PREMIUM", "M", "George", "Mo", "James", "GMJ", "", "", "1999-01-20", "2022-01-02")
+
+        and: 'address of these customers'
+        def address1 = new Address("addr-123", "cust-123", "cd", "HOME", "111 1st St", "Apt 2", "Madison", "WI", "55555", "1233", "USA", 0L, 0L)
+        def address2 = new Address("addr-456", "cust-456", "cd", "HOME", "111 1st St", "Apt 2", "Seattle", "WA", "55553", "1234", "USA", 0L, 0L)
+        def address3 = new Address("addr-567", "cust-567", "cd", "HOME", "111 21st St", "Apt 3", "Madison", "WI", "55554", "1235", "USA", 0L, 0L)
+
+        and: 'address of events'
+        def address4 = new Address("addr-789", null, "cd", "HOME", "111 1st St", "Apt 2", "Austin", "TX", "55556", "1236", "USA", 0L, 0L)
+
+        and: 'venues of events'
+        def venue1 = new Venue("ven-123", "addr-789", "Armoury", 1000)
+
+        and: 'event happening'
+        def event1 = new Event("ev-123", "artist-2", "ven-123", 10, "today")
+
+        and: 'Tickets of customers'
+        def ticket1 = new Ticket("tkt-123", "cust-123", "ev-123", 200)
+        def ticket2 = new Ticket("tkt-456", "cust-456", "ev-123", 200)
+        def ticket3 = new Ticket("tkt-456", "cust-567", "ev-123", 200)
+
+
+        when: 'piping inputs through the stream'
+        customerInputTopic.pipeInput(cust1.id(), cust1)
+        customerInputTopic.pipeInput(cust2.id(), cust2)
+        customerInputTopic.pipeInput(cust3.id(), cust3)
+
+        addressInputTopic.pipeInput(address1.id(), address1)
+        addressInputTopic.pipeInput(address2.id(), address2)
+        addressInputTopic.pipeInput(address3.id(), address3)
+
+        addressInputTopic.pipeInput(address4.id(), address4)
+
+        venueInputTopic.pipeInput(venue1.id(), venue1)
+
+        eventInputTopic.pipeInput(event1.id(), event1)
+
+
+
+//        ticketInputTopic.pipeInput(ticket1.id(), ticket1)
+//        ticketInputTopic.pipeInput(ticket2.id(), ticket2)
+//        ticketInputTopic.pipeInput(ticket3.id(), ticket3)
+
+        and: 'reading the output records'
+        def outputRecords = outputTopic.readRecordsToList()
+
+        then: 'the expected number of records were received'
+        outputRecords.size() == 1
+    }
+}
+
